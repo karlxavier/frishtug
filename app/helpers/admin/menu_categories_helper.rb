80086209468b02c8c5968c8c3c3622475f920cfd
James Robert Rooke <thepoltergeist23@gmail.com>
2018-01-17 11:46:10 +0800

    display add-ons and add a link to add an add-ons for a category without one

--- app/helpers/admin/menu_categories_helper.rb
+++ app/helpers/admin/menu_categories_helper.rb
@@ -1,2 +1,18 @@
 module Admin::MenuCategoriesHelper
+  def display_all_add_ons(menu_category)
+    return no_add_ons if menu_category.add_ons.blank?
+    content_tag :span, class: 'd-block', style: truncatable_style do
+      menu_category.add_ons.map(&:name).join(', ')
+    end
+  end
+
+  private
+
+  def no_add_ons
+    content_tag :small, 'No Add-Ons', class: 'text-muted font-size-12'
+  end
+
+  def truncatable_style
+    'width: 18rem; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;'
+  end
 end
