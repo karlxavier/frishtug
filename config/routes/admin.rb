62ff965509b905965426e9917590bb5e0d5212e8
James Robert Rooke <thepoltergeist23@gmail.com>
2018-02-18 16:07:40 +0800

    fix import menus link

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -8,7 +8,9 @@ namespace :admin do
   resources :menu_categories, except: :show
   resources :plans
   resources :allowed_zip_codes, except: :show
-  resources :import_menus, only: :index
+  resources :import_menus, only: :index do
+    post :import, on: :collection
+  end
   resources :shoppings_lists
   resources :configs
   resources :inventory, only: :index
