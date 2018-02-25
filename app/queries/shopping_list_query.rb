dbeef074b1d951136727fbb4a94a568a5b3b833c
James Robert Rooke <thepoltergeist23@gmail.com>
2018-02-25 14:12:46 +0800

    update ui

--- app/queries/shopping_list_query.rb
+++ app/queries/shopping_list_query.rb
@@ -5,7 +5,7 @@ class ShoppingListQuery
 
   def get_list
     list = []
-    Menu.all_published.includes(orders: :menus_orders).map do |menu|
+    Menu.all_published.map do |menu|
       orders = menu.orders.placed_between?(range)
       list << format_result(orders, menu) if orders.present?
     end
