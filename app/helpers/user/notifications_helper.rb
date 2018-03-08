b96401fd4819acae3c02798e46b511c70bec8e33
James Robert Rooke <thepoltergeist23@gmail.com>
2018-03-08 12:50:12 +0800

    add notification

---
+++ app/helpers/user/notifications_helper.rb
@@ -0,0 +1,13 @@
+module User::NotificationsHelper
+  def display_incomplete_plan_notification
+    return nil if current_user.plan.interval != 'month'
+    return nil if current_user.orders.count >= 20
+    remaining_days = 20 - current_user.orders.count
+    remaining_weeks = remaining_days / 5
+    orders_to_complete = remaining_weeks.zero? ?
+      pluralize(remaining_days, 'day') : pluralize(remaining_weeks, 'week')
+    content_tag :div, class: 'alert alert-info notifications' do
+      "You must complete your plan, you still have #{orders_to_complete} to fill."
+    end
+  end
+end
