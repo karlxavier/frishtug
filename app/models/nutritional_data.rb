e90168f828b600a1fa0b628488b12f6f5ed2804c
karlxavier <kxdc1025@yahoo.com>
2018-05-02 21:06:57 +0400

    nutritional data modal popup in user registration

--- app/models/nutritional_data.rb
+++ app/models/nutritional_data.rb
@@ -1,3 +1,5 @@
 class NutritionalData < ApplicationRecord
   belongs_to :menu
+
+  scope :nutri_data, -> (menu_id) { select('nutritional_data.id', 'menus.name AS menu_name', 'nutritional_data.data', 'nutritional_data.menu_id').where(menu_id: menu_id).joins(:menu) }
 end
