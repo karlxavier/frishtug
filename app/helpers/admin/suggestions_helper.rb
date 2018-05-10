2f430b36c2a096ec3a5c81d2195ee1e2c8dcc7a7
James Robert Rooke <thepoltergeist23@gmail.com>
2018-05-10 14:02:04 +0800

    fix ui

--- app/helpers/admin/suggestions_helper.rb
+++ app/helpers/admin/suggestions_helper.rb
@@ -3,9 +3,9 @@ module Admin::SuggestionsHelper
     commentable = comment.commentable
     case comment.commentable_type
     when "Order"
-      return link_to "Order ##{commentable.id}", admin_order_path(commentable.id)
+      return link_to "Order ##{commentable.id}", admin_order_path(commentable.id), class: 'chocolate-font-color'
     when "Plan"
-      return link_to "Plan #{commentable.name}", admin_plan_path(commentable.id)
+      return link_to "Plan #{commentable.name}", admin_plan_path(commentable.id), class: 'chocolate-font-color'
     end
   end
 end
