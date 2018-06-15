f6dd180a95ed7762abac413ecc0201855eeafa34
James Robert Rooke <thepoltergeist23@gmail.com>
2018-06-15 10:11:05 +0800

    fix saving of orders

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -16,7 +16,8 @@ class Ledger < ApplicationRecord
   enum status: %i[pending_payment paid payment_failed]
   belongs_to :order
   belongs_to :user
-  after_save :destroy_zero_amount, :update_order
+  after_save :update_order
+  after_update :destroy_zero_amount
 
   def self.unpaid
     where.not(status: :paid)
@@ -35,6 +36,9 @@ class Ledger < ApplicationRecord
   end
 
   def update_order
+    return if ['pending_payment', 'payment_failed'].include?(self.status)
+    return if self.amount.nil?
+    return if self.amount <= 0
     types = { "TaxLedger" => "tax", "AdditionalLedger" => "excess"}
     order = Order.find(self.order_id)
     RecordPayments.call(order, self.amount, types[self.type])
