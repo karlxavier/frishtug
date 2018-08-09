a6f9bf4b0515e6c64c4a61fb140b0585c91fe59b
James Robert Rooke <thepoltergeist23@gmail.com>
2018-08-09 16:19:20 +0800

    rescue plan without image display option4 images instead

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -1,8 +1,13 @@
 module AssetHelper
-
   def asset_image_tag(asset, options = {})
     options[:fetch_format] = :auto
     options[:quality] = :auto
     cl_image_tag asset.image, options
+  end
+
+  def plan_image_path(plan)
+    asset_pack_path "packs/images/#{p.name.downcase.split(" ").join}.png"
+  rescue
+    asset_pack_path "packs/images/option4.png"
   end
 end
