20cade5f2d0ba8fe7941512bd217a17377d4372e
James Robert Rooke <thepoltergeist23@gmail.com>
2018-08-23 11:30:10 +0800

    add cancel orders checker if saturday or blackout date

--- app/controllers/admin/cancel_orders_controller.rb
+++ app/controllers/admin/cancel_orders_controller.rb
@@ -1,6 +1,14 @@
 class Admin::CancelOrdersController < Admin::BaseController
+  SKIPPABLE_DATES = BlackoutDate.pluck_dates.freeze
   def create
-    CancelOrdersWorker.perform_async
-    redirect_to admin_dashboard_index_path, notice: 'Processing cancellation of orders.'
+    current_time = Time.current
+    if current_time.saturday?
+      redirect_to admin_dashboard_index_path, notice: 'Nothing to cancel for saturday'
+    elsif SKIPPABLE_DATES.include?(current_time.strftime("%B %d"))
+      redirect_to admin_dashboard_index_path, notice: 'Nothing to cancel for Blackout date'
+    else
+      CancelOrdersWorker.perform_async
+      redirect_to admin_dashboard_index_path, notice: 'Processing cancellation of orders.'
+    end
   end
 end
