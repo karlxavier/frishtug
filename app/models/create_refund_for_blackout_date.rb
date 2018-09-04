efe2ef1d1c77cac49d24ab28d8b16a946cca5b86
James Robert Rooke <thepoltergeist23@gmail.com>
2018-09-04 10:07:01 +0800

    blackout date refund add description

--- app/models/create_refund_for_blackout_date.rb
+++ app/models/create_refund_for_blackout_date.rb
@@ -15,6 +15,13 @@ class CreateRefundForBlackoutDate
 
   private
 
+  def blackout_date_description
+    BlackoutDate.pluck(:month, :day, :description).reduce({}) do |h, value|
+      h["#{value.first} #{value.second}"] = value.last
+      h
+    end
+  end
+
   def valid?
     return false if total <= 0
     return false unless BLACKOUT_DATES.include?(order.placed_on&.strftime("%B %d"))
@@ -44,7 +51,7 @@ class CreateRefundForBlackoutDate
         amount: total,
         activation_date: Time.current,
         charge_id: charge_id,
-        remarks: "Black-out date #{order.placed_on&.strftime("%B %d, %Y")}",
+        remarks: "Black-out date #{order.placed_on&.strftime("%B %d, %Y")}, #{blackout_date_description[order.placed_on.strftime('%B %d')]}",
       )
     end
   end
