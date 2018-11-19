0bcd18dee187667a61e1cc0acd4f62db7226a0c5
James Robert Rooke <thepoltergeist23@gmail.com>
2018-11-19 16:10:28 +0800

    update polling of order status

--- app/controllers/user/scanovators_controller.rb
+++ app/controllers/user/scanovators_controller.rb
@@ -2,8 +2,13 @@ class User::ScanovatorsController < User::BaseController
   respond_to :js, only: :index
 
   def index
-    @response = ScanovatorApi.fetch(params[:order_id])
-    @response = NullScanovator.new if @response.state == 'fail'
+    order = Order.find(params[:order_id])
+    if order.present && order.is_rollover?
+      @response = ScanovatorApi.fetch(params[:order_id])
+      @response = NullScanovator.new if @response.state == 'fail'
+    else
+      @response = NullScanovator.new
+    end
     respond_with(@response)
   end
 end
